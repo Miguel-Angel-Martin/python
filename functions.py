197cd4191423a96219fbe193131a1b75f84d1164
miguel-angel martin <miguel-angel.martin@avl.com>
2022-09-19 13:24:42 +0200

    read me

--- functions.py
+++ functions.py
@@ -13,4 +13,4 @@ def welcome(name):
 
 welcome("Miguel")
 
-# https://www.freecodecamp.org/espanol/news/python-ejemplos-de-codigo-tutorial-de-programacion-en-python-desde-cero-para-principiantes/#-funciones-en-python
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Python
+
+ [Course](https://www.freecodecamp.org/espanol/news/python-ejemplos-de-codigo-tutorial-de-programacion-en-python-desde-cero-para-principiantes/#-funciones-en-python)
