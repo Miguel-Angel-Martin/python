cf26e460b4e0791408bb7538b6a541d2335270c3
miguel-angel martin <miguel-angel.martin@avl.com>
2022-11-08 12:09:46 +0100

    start again the first course

---
+++ 01_computer_science/01_computation/computation.py
@@ -0,0 +1,13 @@
+''' This program summation of two numbers and displays their results '''
+PI = 3.14159
+RADIOUS = 2.2
+# area of circle equation <- this is a comment
+AREA = PI*(RADIOUS**2)
+print(AREA)
+
+# change values of radius <- another comment
+# use comments to help others understand what you are doing in code
+RADIOUS = RADIOUS + 1
+print(AREA)     # area doesn't change
+AREA = PI*(RADIOUS**2)
+print(AREA)
--- 01_computer_science/02-Branching_Iteration/iteration.py
+++ 01_computer_science/02-Branching_Iteration/iteration.py
@@ -1,15 +1,14 @@
 ###################
 ## EXAMPLE: strings 
 ###################
-#name = "ana"
 #hi = "hello there"
+#name = "ana"
 #greet = hi + name  
 #print(greet)
 #greeting = hi + " " + name
 #print(greeting)
 #silly = hi + (" " + name)*3
 #print(silly)
-
 
 ####################
 ## EXAMPLE: output 
